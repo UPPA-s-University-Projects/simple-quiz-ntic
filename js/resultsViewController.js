4e8f04e0dbba927b2fd8db0aab9dd5b3833d4045
Ttiki <clem64.combier@gmail.com>
2022-04-24 20:03:30 +0200

    fix2

--- js/resultsViewController.js
+++ js/resultsViewController.js
@@ -3,7 +3,7 @@ var qas = JSON.parse(localStorage.getItem("qas"));
 //foeach entry in the qas, create an object of QA
 var qaList = [];
 for (var i = 0; i < qas.length; i++) {
-    var qa = new QA(qas[i].question, qas[i].answer, qas[i].imgURI, qas[i].userAnswer);
+    var qa = new QA(jsonObj[i].question, jsonObj[i].answer, jsonObj[i].answer2, jsonObj[i].img);
     qaList.push(qa);
 }
 
