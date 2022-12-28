8e0674359c83d3056298b9bf784c29545e5f5952
Ttiki <clem64.combier@gmail.com>
2022-12-28 14:06:23 +0100

    Added new questions

---
+++ res/codes_ex/Abstraction.java
@@ -0,0 +1,22 @@
+public abstract class Animal {
+    protected String name;
+    protected int age;
+
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public abstract void makeSound();
+}
+
+public class Dog extends Animal {
+    public Dog(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Woof!");
+    }
+}
